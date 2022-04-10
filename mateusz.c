623a840a35e9b7d0e4cdbd5773f0f426c27b6942
TheRipSon <mat0007@wp.pl>
2022-04-10 15:26:05 +0000

    ./wake z bomby budzi proses ./mateusz //kwestia zmiany komentarzy by budzil ./SyncMain , dodatkowa obcja budzenia dowolnego pid poprzez parametr ./wake pid

--- mateusz.c
+++ mateusz.c
@@ -2,14 +2,26 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
-void sig_handler_parent(int signum){
-  printf("Parent : Received a response signal from child \n");
+
+static volatile sig_atomic_t sig = 0;
+
+void sig_handler_sigusr1(int signum){
+  sig=signum;
+}
+
+int toSleep(int time){
+  signal(SIGUSR1,sig_handler_sigusr1); // Register signal handler
+  printf("Sleep\n");
+  sleep(time);
+  int status;
+  if(sig!=0)
+    printf("Wake up after SIGUSR1\n");
+  else
+    printf("Wake up after %ds\n",time);
+  return 0;
 }
 
 int main(){
-  signal(SIGUSR1,sig_handler_parent); // Register signal handler
-  sleep(1);
-  printf("Parent: waiting for response\n");
-  pause();
+  toSleep(10);
   return 0;
 }
--- wake.c
+++ wake.c
@@ -3,8 +3,15 @@
 #include <stdio.h>
 int main(int argc,char * argv[])
 {
-  int pid  = atoi(argv[1]);
+  char buf[512];
+  //FILE *cmd_pipe = popen("pidof -s SyncMain", "r");
+  FILE *cmd_pipe = popen("pidof -s mateusz", "r");
+  fgets(buf, 512, cmd_pipe);
+  pid_t pid = strtoul(buf, NULL, 10);
+  pclose( cmd_pipe );
+  if(argc>1)
+    pid  = atoi(argv[1]);
   kill(pid, SIGUSR1);
-  printf("Pobudzono proces o pidid: %d\n",pid);
+  printf("Send SIGUSR1 to pid: %d\n",pid);
   return 0;
 }
